86997870f9691a904cf0f17827f11d8b28d8876f
emts2002 <emts2002@gmail.com>
2023-01-08 18:34:33 +0000

    fixed bugs

--- main.cpp
+++ main.cpp
@@ -15,15 +15,17 @@ Graph readAirports(hTable& hT1) { // O(V)
     ifstream infile("data/airports.csv");
     string line;
     bool first = true;
-    while (getline(infile, line, ',')) {
+    while (getline(infile, line)) {
         if (first) { first = false;}
         else {
             for (int i = 0; i < line.length(); ++i) {
+                if (line[i] == ' ') {
+                    line[i] = '_';
+                }
+            }
+            for (int i = 0; i < line.length(); ++i) {
                 if (line[i] == ',') {
                     line[i] = ' ';
-                }
-                else if (line[i] == ' ') {
-                    line[i] = '_';
                 }
             }
             istringstream iss(line);
@@ -31,6 +33,21 @@ Graph readAirports(hTable& hT1) { // O(V)
             float Latitude,Longitude;
             iss >> Code >> Name >> City >> Country >> Latitude >> Longitude;
             n++;
+            for (int i = 0; i < Name.length(); ++i) {
+                if (Name[i] == '_') {
+                    Name[i] = ' ';
+                }
+            }
+            for (int i = 0; i < City.length(); ++i) {
+                if (City[i] == '_') {
+                    City[i] = ' ';
+                }
+            }
+            for (int i = 0; i < Country.length(); ++i) {
+                if (Country[i] == '_') {
+                    Country[i] = ' ';
+                }
+            }
             Airport ap = Airport(n, Code, Name, City, Country, Latitude, Longitude);
             hT1[ap.getCode()] = ap;
         }
@@ -39,27 +56,48 @@ Graph readAirports(hTable& hT1) { // O(V)
     return g;
 }
 
-void readAirlines() { // O(n)
+void readAirlines(vector<Airline> & airlines) { // O(n)
     ifstream infile("data/airlines.csv");
     string line;
     bool first = true;
-    while (getline(infile, line, ',')) {
+    while (getline(infile, line)) {
         if (first) { first = false;}
         else {
+            for (int i = 0; i < line.length(); ++i) {
+                if (line[i] == ' ') {
+                    line[i] = '_';
+                }
+            }
+            for (int i = 0; i < line.length(); ++i) {
+                if (line[i] == ',') {
+                    line[i] = ' ';
+                }
+            }
             istringstream iss(line);
             string Code,Name,Callsign,Country;
             iss >> Code >> Name >> Callsign >> Country;
 
+            for (int i = 0; i < Name.length(); ++i) {
+                if (Name[i] == '_') {
+                    Name[i] = ' ';
+                }
+            }
+            for (int i = 0; i < Country.length(); ++i) {
+                if (Country[i] == '_') {
+                    Country[i] = ' ';
+                }
+            }
             Airline al = Airline(Code, Name, Callsign, Country);
+            airlines.push_back(al);
         }
     }
 }
 
-void readFlights(Graph g, hTable hT) { // O(E)
+void readFlights(Graph &g, hTable &hT) { // O(E)
     ifstream infile("data/flights.csv");
     string line;
     bool first = true;
-    while (getline(infile, line, ',')) {
+    while (getline(infile, line)) {
         if (first) { first = false;}
         else {
             istringstream iss(line);
@@ -80,9 +118,11 @@ Airport getClosest(Airport ap, Graph g, hTable hT) { // O(E)
 
 int main() {
     hTable hT1;
+    Graph g;
+    vector<Airline> airlines;
     readAirports(hT1);
-    /*if(hT1.empty()) return 1;*/
-    Airport ap = hT1["JFK"];
-    cout << ap.getName();
+    readAirlines(airlines);
+    readFlights(g, hT1);
+    //cout << g.airlinesFlyingFromAirport(10);
     return 0;
 }
