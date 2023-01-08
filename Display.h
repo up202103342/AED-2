e6f5934997bdb039033b26310a99e0b13dde1145
up202108686 <up202108686@up.pt>
2023-01-08 21:22:38 +0000

    fixed iteration localtolocal

---
+++ Display.h
@@ -0,0 +1,75 @@
+#ifndef AED2_DISPLAY_H
+#define AED2_DISPLAY_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <list>
+
+#include "Graph.h"
+
+using namespace std;
+
+class Display {
+    string state;
+    int numPicked;
+    string letterPicked;
+    float coordLat1;
+    float coordLon1;
+    float coordLat2;
+    float coordLon2;
+    string apCode1;
+    string apCode2;
+
+private:
+    string city1;
+    string city2;
+
+public:
+    Display();
+
+    void displayMenu();
+
+    void processInput(string input);
+
+    void displayLocais();
+
+    void displayLocalPartida();
+
+    void displayLocalChegada();
+
+    void displayVoo();
+
+    void displayInfo();
+
+    void displayAbout();
+
+    //Getters n' Setters
+    string getState() const;
+    void setState(string state);
+    const int &getNumPicked() const;
+    void setNumPicked(const int &numPicked);
+    const string &getLetterPicked() const;
+    void setLetterPicked(const string &letterPicked);
+    float getCoordLat1() const;
+    float getCoordLon1() const;
+    float getCoordLat2() const;
+    float getCoordLon2() const;
+    const string &getApCode1() const;
+    const string &getApCode2() const;
+    const string &getCity1() const;
+    const string &getCity2() const;
+    void setApCode1(const string &apCode1);
+    void setApCode2(const string &apCode2);
+    void setCoordLat1(const float &coordLat1) ;
+    void setCoordLon1(const float &coordLon1) ;
+    void setCoordLat2(const float &coordLat2) ;
+    void setCoordLon2(const float &coordLon2) ;
+
+    void reset();
+};
+
+
+#endif //AED2_DISPLAY_H
